48256410eccb8f7f0ccceef10d2e02d67d921923
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 15:45:42 +0200

    Python - Összehasonlító Operátorok - Gyakorló 1

---
+++ osszehasonlitoOperatorok1/osszehasonlitoOperatorok1.py
@@ -0,0 +1,15 @@
+'''Összehasonlító operátorok:
+<       kisebb
+>       nagyobb
+<=      kisebb, vagy egyenlő
+>=      nagyobb, vagy egyenlő
+==      egyenlő
+!=      nem egyenlő
+'''
+
+szam_a = int(input('Mondj egy egész számot!\n>>>'))
+szam_b = int(input('Mondj még egy egész számot!\n>>>'))
+if szam_a == szam_b:
+    print('A két szám egyenlő!')
+else:
+    print('A két szám nem egyenlő!')
