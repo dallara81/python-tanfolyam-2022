6bdf57430147adc47a4da7b6b372e030f31d521d
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 21:23:49 +0200

    Python - Tanfolyam Lecke - 3.5. Gyakorlás: szendvicsreceptfüggvény

---
+++ tanfolyamLeckeSzendvicsreceptfuggveny/tanfolyamLeckeSzendvicsreceptfuggveny.py
@@ -0,0 +1,15 @@
+'''
+Tanfoilyam - 3.5. Gyakorlás: szendvicsreceptfüggvény
+'''
+
+
+def print_sandwich_recipe(souce_type, meat_type, chese_type, bullshit_type):
+    print('1. Fogj két szelet kenyeret.')
+    print('2. Az egyik szeletre nyomj ' + souce_type + '-t')
+    print('3. Helyezz a kenyérre két szelet ' + meat_type + '-t')
+    print('4. Rakj rá egy szelet ' + chese_type + '-ot')
+    print('5. Koronázd meg pár szelet/karika/csipet ilyen zöldséggel: ' + bullshit_type + '.')
+    print('6. Borítsd be a szendvicset a másik szelet kenyérrel.')
+    print('7. Jó étvágyat!')
+
+print_sandwich_recipe('szósz', 'hús', 'sajt', 'marhalegelőfű')
