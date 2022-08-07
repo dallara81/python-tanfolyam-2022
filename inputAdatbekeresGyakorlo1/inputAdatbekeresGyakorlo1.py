e08db7acbf09aed1531a80984d13a14647ea144d
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 16:01:45 +0200

    Python - Input - Adatbekérés Gyakorló 1

--- inputAdatbekeresGyakorlo1/inputAdatbekeresGyakorlo1.py
+++ inputAdatbekeresGyakorlo1/inputAdatbekeresGyakorlo1.py
@@ -6,5 +6,5 @@ softwareversion = ' v1.2'
 print('Pénz' + 'eső ' + thisthesoftware + softwareversion) # Kiiratás
 
 osszesadatod = input('Add oda az összes adatod! Bennem megbízhatsz.\nBizalommal: Mustafa Goldwin, Nigéria\n>>>') # string input
-print('\nKüldj 1000 dollárt a 3434-0013-4432-1242 bankszámlára, különben publikálom minden adatodat!\nNem viccelek, mindent tudok rólad:')
+print('\nKüldj 1000 dollárt a 3434-0013-4432-1242 bankszámlára, különben nigériai módon kezelem az adataidat!\nNem viccelek, mindent tudok rólad:')
 print(osszesadatod)
