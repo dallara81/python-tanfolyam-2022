1ccd375a2ad79b0deee24a91a6b996e15e8934b5
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 15:55:08 +0200

    Python - Adattípusok - Adattípusok Kiiratása Gyakorló 1

---
+++ adattipusokKiiratasaGyakorlo1/adattipusokKiiratasaGyakorlo1.py
@@ -0,0 +1,25 @@
+'''
+Gyakorlás - Python alapok - Típusok kiíratása
+... és mert nem csak #-el lehet kommentelni!
+'''
+
+szoveg = "izé. Ennyi."
+szam = 88 # mert az egy jó szám!
+tizedes_tort = 0.7
+oszthatoe = False
+
+print('Szöveg:')
+print(szoveg)
+print(type(szoveg))
+
+print('\nSzám:')
+print(szam)
+print(type(szam))
+
+print('\nTizedes tört:')
+print(tizedes_tort)
+print(type(tizedes_tort))
+
+print('\nOsztható-e?')
+print(oszthatoe)
+print(type(oszthatoe))
