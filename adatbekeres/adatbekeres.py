1405a0fb4864b25e52a45edfd4e87932c1e0f2a5
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 15:06:00 +0200

    Adatbekérő v1.2 - ahol minden adatod jó helyen lesz

--- adatbekeres/adatbekeres.py
+++ adatbekeres/adatbekeres.py
@@ -5,6 +5,6 @@ thisthesoftware = 'program'
 softwareversion = ' v1.2'
 print('Pénz' + 'eső ' + thisthesoftware + softwareversion)
 
-osszesadatod = input('Add oda az összes adatod! Bennem megbízhatsz.\nBizalommal: Mustafa Goldwin, Nigéria\n>>>')
+osszesadatod = input('Add oda az összes adatod! Bennem megbízhatsz.\nBizalommal: Mustafa Goldwin, Nigéria\n>>>') # string input
 print('\nKüldj 1000 dollárt a 3434-0013-4432-1242 bankszámlára, különben publikálom minden adatodat!\nNem viccelek, mindent tudok rólad:')
 print(osszesadatod)
