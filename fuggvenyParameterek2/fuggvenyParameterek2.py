558eabf789bcfa8af3f5e35aaa5a9ec4363c6584
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 22:30:57 +0200

    Python - Függvények - Függvény Paraméterek - Gyakorlás 2.

---
+++ fuggvenyParameterek2/fuggvenyParameterek2.py
@@ -0,0 +1,20 @@
+'''
+Függvény paraméterek - Gyakorlás 2.
+--- ISA Types ---
+'''
+
+def isa_types(isa_Type1, isa_Type2, isa_Type3, isa_Type4, isa_Type5, isa_Type6, isa_Type7, isa_Type8, isa_Type9):
+    print('Sokféle számítógéparchitektúra létezik az ' + isa_Type1 + '-en felül.')
+    print('Hosszú lenne mind felsorolni, de kezdjük talán a legkézenfekvőbbel, hisz mindannyiunk zsebében ott lapul:')
+    print('a mobiltelefonokban ketyegő rendszercsipek ' + isa_Type2 + 'utasításkészlete ma már sokmilliárdnyi eszközben találhaó meg.')
+    print('Sokféle utasítékészlet története ért véget évekkel, vagy évtizedekkel ezelőtt - velük így most nem foglalkozunk.')
+    print('A ma is használatban lévő ISA-k közt említést érdemel a leginkább beágyazott rendszerekhez használt ' + isa_Type3 + '.')
+    print('A ' + isa_Type3 + ' mára összeolvadt a szerveres vonal ' + isa_Type4 + ' architektúrával.')
+    print('Érdekességképp megjegyezhetjük, hogy a ' + isa_Type4 + ' architektúrát az IBM néhány évvel ezelőtt szabaddá tette ' + isa_Type5 + ' néven,')
+    print('azzal a kikötéssel, hogy annak legalább 3-as főverzióval történt megvalósításai mentesülnek a jogdíjfizetés alól.')
+    print('Bár manapság már leáldozóban van a csillaga, de főleg szervereknél széles körben használt architektúra a ' + isa_Type6 + '.')
+    print('Ezeken felül említést érdemel még a ' + isa_Type7 + ' és az ' + isa_Type8 + ' is.')
+    print('Ha pedig a jövőre tekintnk, mindenképp a ' + isa_Type9 + '-re kell odafigyelnünk.')
+    print('A ' + isa_Type9 + ' könnyű szerrel egymaga leválthatja az összes, fent említett számítógéparchitektúrát a jövőben.')
+
+isa_types('x86/64x', 'ARM v8/v9', 'PowerPC', 'POWER', 'OpenPOWER', 'SPARC', 'MIPS', 'Elbrus', 'RISC-V')
