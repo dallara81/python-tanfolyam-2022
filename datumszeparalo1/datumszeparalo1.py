5688e33473c16ce62c4b3bdcc79224f13a9d7b97
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 15:30:06 +0200

    Python - Print - Dátum(sep=)aráló 1

---
+++ datumszeparalo1/datumszeparalo1.py
@@ -0,0 +1,14 @@
+'''Dátumszeparáló szeparátor'''
+
+szuletesi_ev = 1981
+szuletesi_honap = 1
+szuletesi_nap = 1
+
+print(szuletesi_ev, szuletesi_honap, szuletesi_nap, sep='.')
+print('Január elseje, mint minden "tanult" migránsé a fejlett Nyugaton!')
+
+print('\nSoronként is megy?')
+print(szuletesi_ev, szuletesi_honap, szuletesi_nap, sep='\n')
+
+print('\nDe én kötőjeleket akarok!')
+print(szuletesi_ev, szuletesi_honap, szuletesi_nap, sep='-')
