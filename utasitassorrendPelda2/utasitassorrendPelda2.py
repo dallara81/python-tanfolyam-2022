09a1aac317018627cfd10842c232f99e7d702b78
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 19:29:33 +0200

    Python - Utasítássorend - Példa 2.

---
+++ utasitassorrendPelda2/utasitassorrendPelda2.py
@@ -0,0 +1,19 @@
+answer = 'Ez egy válasz.'
+
+def some_function():
+    print('és')
+
+print(12 + 2)
+
+some_function()
+
+print(42)
+
+some_function()
+
+print(14 * 42)
+
+"""
+Control flow (utassítássorrend) az alábbiak szerint alakul:
+ 1. → 3. → 6. → 8. → 3. → 4. → 8. → 10. → 12. → 3. → 4. → 12. → 14.
+"""
