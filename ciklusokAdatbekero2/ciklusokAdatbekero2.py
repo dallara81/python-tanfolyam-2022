6086a52c2f49cf989d87b12e630e2144c74f798d
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 15:16:29 +0200

    Python - Ciklusok - Adatbekérő 2

---
+++ ciklusokAdatbekero2/ciklusokAdatbekero2.py
@@ -0,0 +1,12 @@
+'''
+Ciklusok - Adatbekérő 2
+(egyszerű gyakorlópélda)
+Kilépés az ENTER megnyomására
+'''
+
+
+word = None
+while word != '':       # Üres string, praktikusan csak egy ENTER megnyomására
+    word = input('Gépelj kislány! Kilépés gépelés nélkül az ENTER megnyomására.\n >>>')
+
+#print(type(word))
