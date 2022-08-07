85b066bca8346c2cfc660e9fafebe4d819c08ee5
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 18:21:52 +0200

    Python - Függvények - Függvény definiálása és meghívása

---
+++ fuggvenyDeklaralasaGyakorlo1/fuggvenyDeklaralasaGyakorlo1.py
@@ -0,0 +1,18 @@
+'''
+Függvénydeklaráció
+(gyakorlópélda)
+'''
+
+
+# definiálom a gyakorlófüggvényt:
+def print_tomato_soup_recipe():
+    print('Pirítsd meg olívaolajon a hagymát és fokhagymát.')
+    print('Tedd hozzá a felkockázott paradicsomot, sózd és borsozd meg, majd öntsd fel 2 dl vízzel.')
+    print('Főzd kis lángon 15-20 percig.')
+    print('Ha szétfőtt a paradicsom, add hozzá a száraz kenyeret, olívaolajat, bazsalikomot, és adj hozzá egy kis vizet.')
+    print('Főzd még 5 percig.')
+    print('Szórj rá reszelt parmezánt.')
+    print('Szolgáld fel ciabattával.')
+
+# meghívom a függvényt:
+print_tomato_soup_recipe()
