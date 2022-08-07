dc90803f366a6e3a0de15ddbd6df87ce08102dae
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 15:24:46 +0200

    Python - Ciklusok - Ciklusok Számláló 1

---
+++ ciklusokSzamlalo1/ciklusokSzamlalo1.py
@@ -0,0 +1,10 @@
+'''
+Ciklusok - Számláló
+(egyszerű példa, két alváltozatban)
+'''
+
+szamlalo = 1
+while szamlalo <= 5:
+    print('Lefutott (még) egyszer.')
+    #szamlalo = szamlalo + 1
+    szamlalo += 1 # Mégis csak elegánsabb így
