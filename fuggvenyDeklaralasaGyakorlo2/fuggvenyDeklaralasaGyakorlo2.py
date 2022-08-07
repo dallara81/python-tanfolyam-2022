8d855e02c62b8dcd7bfcff3e6c0d4cdec64fd1b4
dallara81 <ferenc.szilvai@yahoo.se>
2022-08-07 18:39:55 +0200

    Python - Függvények - Függvény definiálása és meghívása 2.

---
+++ fuggvenyDeklaralasaGyakorlo2/fuggvenyDeklaralasaGyakorlo2.py
@@ -0,0 +1,26 @@
+'''
+Függvénydeklaráció 2.
+(gyakorlópélda)
+'''
+
+print('Favoritrecept Szuperszoftver v6.9\n')
+
+def print_favourite_recipe1():
+    print('Kinyitom a söröm és megiszom.')
+
+def print_favourite_recipe2():
+    print('Kinyitok még egy sört és megiszom.')
+
+print_favourite_recipe1()
+
+print('\nRepeat!')
+
+print_favourite_recipe2()
+
+print('\nNaná, hogy repeat!')
+
+print_favourite_recipe2()
+
+print('\nNegyedszer is!')
+
+print_favourite_recipe2()
